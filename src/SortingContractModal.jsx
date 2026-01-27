515e0bb1308fd205da7287573bf52d95690b1b5d
constantiniulianneagu-lab <constantiniulian.neagu@gmail.com>
2026-01-27 13:30:45 +0200

    1

--- src/SortingContractModal.jsx
+++ src/SortingContractModal.jsx
@@ -266,7 +266,7 @@ const SortingContractModal = ({
               <option value="">Selectează sector...</option>
               {sectors.map(s => (
                 <option key={s.id} value={s.id}>
-                  Sector {s.sector_number} - {s.sector_name}
+                  {s.sector_name}
                 </option>
               ))}
             </select>
